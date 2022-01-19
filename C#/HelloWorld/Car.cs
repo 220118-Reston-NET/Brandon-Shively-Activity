d7525589ee881d1be0c9b556fc2fe942ec268654
bshively2 <bshively76@gmail.com>
2022-01-19 15:04:05 -0500

    Converting Shell Script to C#

--- C#/HelloWorld/Car.cs
+++ C#/HelloWorld/Car.cs
@@ -1,25 +1,70 @@
-//Namespaces organize code very well
+// //Namespaces organize code very well
+
 namespace CarFunction
 {
-    //This is how to create a class using class keyword
-//Public makes the class visible to everything
-    public class Car
+//     //This is how to create a class using class keyword
+// //Public makes the class visible to everything
+ public class Car
+     {
+
+//         //Field
+//         //It is used to store information or define the current state of the object when you first make it
+//         //Private means only the class can use that field
+//         private string _color = "Blue";
+
+         public int GallonPerMile { get; set; }
+
+//         //Property
+//         //They are in PascalCase
+//         public string Color
+//         {
+//             //get keyword in a proprety is a way to get a private field mutable
+//             get{ return "The color is: "+_color; }
+
+//             //set keyword is a preperty is how you can make one of your private fields writable
+//             set{ _color = value; }
+//         }
+
+//         private string _owner; 
+
+//         public string Owner
+//         {
+//             get{ return "The owner is: "+ _owner; }
+//             set{ _owner = value; }
+//         }
+
+         public int Fuel { get; set; }
+
+//         public Car()
+//         {
+//             _color = "Blue";
+//             GallonPerMile = 10;
+//             _owner = "No Owner";
+//             Fuel = 100;
+//         }
+
+//         //A method will run mutiple lines of code to do some sort of operation
+
+//         public void Start()
+//         {
+//             Console.WriteLine("The car is starting now!");
+//             Console.WriteLine($"The current fuel is: {Fuel}");
+//         }
+//         public void Start(int p_fuel)
+//         {
+//             Fuel = p_fuel;
+//             Console.WriteLine("The car is starting now!");
+//             Console.WriteLine($"The current fuel is: {Fuel}");
+//         }
+
+    // Console.WriteLine("Welcome to programming!");
+    // Console.WriteLine("Please tell me your name");
+    // string userInput = Console.ReadLine();
+    // Console.WriteLine($"Hello {userInput}, what do you want to do today?");
+
+    public int TotalDistance()
     {
-
-        //Field
-        //It is used to store information or define the current state of the object when you first make it
-        //Private means only the class can use that field
-        private string _color = "Blue";
-        private string _owner;
-        private int _fuel;
-        private int _gallonPerMile;
-
-        //Property
-        //They are in PascalCase
-        public string Color
-        {
-            get{ return "The color is: "+_color; }
-        }
-
+        return Fuel/GallonPerMile;
+    }
     }
 }
--- C#/HelloWorld/Program.cs
+++ C#/HelloWorld/Program.cs
@@ -1,8 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CarFunction;
+﻿// // See https://aka.ms/new-console-template for more information
+ using CarFunction;
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
-//NameOfClass NameofVariable = new NameOfClass();
-Car car1 = new Car();
-Console.WriteLine(car1.Color);
+// //NameOfClass NameofVariable = new NameOfClass();
+// Car car1 = new Car();
+// Console.WriteLine(car1.Color);
+// Console.WriteLine(car1.GallonPerMile);
+// Console.WriteLine(car1.Owner);
+// Console.WriteLine(car1.Fuel);
+// car1.Start(5);
+
+//  Car car2 = new Car()
+//  {
+//      Fuel = 50,
+//      GallonPerMile = 5,
+//  };
+
+// Console.WriteLine(car2.TotalDistance());
+
+// Console.WriteLine(car2.Color);
+
+Console.WriteLine("Welcome to programming!");
+Console.WriteLine("Please tell me your name");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}, what do you want to do today?");
+
+Console.WriteLine("[1] - Add two numebrs");
+Console.WriteLine("[2] - Exit");
+
+string userInput = Console.ReadLine();
+
+if (userInput == "1")
+{
+    Console.WriteLine("Please give me two numbers");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"The sum is: {num1+num2}");
+}
+
+else if(userInput == "2")
+{
+    
+}
+
+
